ef7b04d13a26958f65c6d5b2fdb195352487846c
BLGALEX <lepehaleha@bk.ru>
2022-02-01 13:58:51 +0000

    added models and some changes in poetry toml

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,69 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings 
+from django.contrib.auth.models import (
+	AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    # Временная метка создания объекта.
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Временная метка показывающая время последнего обновления объекта.
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'username'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def token(self):
+        return self._generate_jwt_token()
+
+    def _generate_jwt_token(self):
+        dt = datetime.now() + timedelta(minutes=5)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token
+
+class TaskList(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    task_list=models.ForeignKey(TaskList, on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
